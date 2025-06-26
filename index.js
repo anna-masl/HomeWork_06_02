463002b02ac07003b3fc69888e05a00380818b7c
anna-masl <anna.maslinnikova@gmail.com>
2025-06-26 19:06:47 +0300

    fix

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const massiveVariable = [1, null, 5, true, undefined, 3];
-let numberOfDigit = 0;
-let sumOfDigit = 0;
+
 
 console.log(massiveVariable);
 
-function calcAverage(massiveVariable){
-    for (let i = 0 ; i < massiveVariable.length ; i++) {
-    if (typeof(massiveVariable[i]) === 'number' || Number.isFinite(massiveVariable[i])){
-      numberOfDigit ++;
+function calcAverage(massiveVariable) {
+  let numberOfDigit = 0;
+  let sumOfDigit = 0;
+  for (let i = 0; i < massiveVariable.length; i++) {
+    if (
+      typeof massiveVariable[i] === 'number' ||
+      Number.isFinite(massiveVariable[i])
+    ) {
+      numberOfDigit++;
       sumOfDigit += +massiveVariable[i];
     }
-  } 
-  return sumOfDigit/numberOfDigit;
+  }
+  return sumOfDigit / numberOfDigit;
 }
 
 console.log(` Average value is ${calcAverage(massiveVariable)} \n Number of Digital is ${numberOfDigit} \n Sum of Digital is ${sumOfDigit}`);
